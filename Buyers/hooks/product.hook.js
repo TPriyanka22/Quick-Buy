5971f9f4a321b1774793bb11b8665f21ac4af1fc
Akhil <akhilthaneti24@.com>
2024-05-17 02:17:33 -0700

    product hook changes

---
+++ Buyers/hooks/product.hook.js
@@ -0,0 +1,62 @@
+import { quickBuyDb } from "@/config/quick-buy";
+import { useState, useEffect } from "react";
+
+
+const useCategoryProducts = (category) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchFromFirestore() {
+      quickBuyDb.collection("Products")
+        .where("category", "==", category)
+        .get()
+        .then(function (querySnapshot) {
+          const products = querySnapshot.docs.map(function (doc) {
+            return { id: doc.id, ...doc.data() };
+          });
+          setData(products);
+          setLoading(false);
+        })
+        .catch((e) => setError(e));
+    }
+
+    fetchFromFirestore();
+  }, [category]);
+
+  return {
+    data,
+    loading,
+    error,
+  };
+};
+
+
+const useProduct = (id) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchFromFirestore() {
+      quickBuyDb.collection("Products")
+        .doc(id)
+        .get()
+        .then(function (doc) {
+          setData(doc.data());
+          setLoading(false);
+        })
+        .catch((e) => setError(e));
+    }
+
+    fetchFromFirestore();
+  }, [id]);
+
+  return {
+    data,
+    loading,
+    error,
+  };
+};
+export { useProduct, useCategoryProducts };
