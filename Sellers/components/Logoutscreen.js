da5a887a513afbe0fed8535294b71ddd103deaf6
Akhil <akhilthaneti24@.com>
2024-05-17 01:39:46 -0700

    logout feature code changes

---
+++ Sellers/components/Logoutscreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import image from '../public/wall.png'
+import styles from '../styles/Logout.module.css'
+const QuickbuyLogoutPage = () => {
+return (
+<div className={styles.container}>
+<p className={styles.title}>Welcome to Quickbuy</p>
+<p className={styles.subtitle}>Please Sign In to access our Quickbuy features</p>
+</div>
+)
+}
+export default QuickbuyLogoutPage
